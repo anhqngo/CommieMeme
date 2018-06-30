89496ad998c1e497e82d9b6ae229f64e04a29e51
Jason Ngo <ngoquocanh99@gmail.com>
2018-06-30 19:49:48 -0400

    Get Url

--- main.py
+++ main.py
@@ -3,6 +3,17 @@ import json
 import urllib2
 
 def check():
-    headers = {"Authorization": "bearer lzaCvprV-UIUR9vVZePY6dEg388", "User-Agent": "CommieMeme/0.1 by jdagod"}
+    headers = {
+        "Authorization": "lzaCvprV-UIUR9vVZePY6dEg388", 
+        "User-Agent": "CommieMeme/0.1 by jdagod",
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',       'Accept-Encoding': 'none',
+       'Accept-Language': 'en-US,en;q=0.8',
+        }
     url = "https://www.reddit.com/r/FULLCOMMUNISM/new/.json"
-    
+    req = urllib2.Request(url, headers=headers)
+    text_data = urllib2.urlopen(req).read()
+    data = json.loads(text_data)
+    pic_url = data["data"]["children"][0]['data']['url']
+    print pic_url
+
+check()
