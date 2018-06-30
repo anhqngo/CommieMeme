784812dcd1940daa9ab7b7e4658bad1a4fbcd9bf
Jason Ngo <ngoquocanh99@gmail.com>
2018-06-30 19:24:06 -0400

    Add reddit headers (including key)

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
+import requests
+import json
+import urllib3
+
 def check():
+    headers = {"Authorization": "bearer lzaCvprV-UIUR9vVZePY6dEg388", "User-Agent": "CommieMeme/0.1 by jdagod"}
     url = "https://www.reddit.com/r/FULLCOMMUNISM/new/.json"
+    
