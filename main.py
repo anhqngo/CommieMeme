3c64520b92607d94bb2a9ea2a114431a59cd8034
Jason Ngo <ngoquocanh99@gmail.com>
2018-06-30 20:43:57 -0400

    Get url

--- main.py
+++ main.py
@@ -6,10 +6,11 @@ def check():
     headers = {
         "Authorization": "lzaCvprV-UIUR9vVZePY6dEg388", 
         "User-Agent": "CommieMeme/0.1 by jdagod",
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',       'Accept-Encoding': 'none',
-       'Accept-Language': 'en-US,en;q=0.8',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+        'Accept-Encoding': 'none',
+        'Accept-Language': 'en-US,en;q=0.8',
         }
-    url = "https://www.reddit.com/r/FULLCOMMUNISM/new/.json"
+    url = "https://www.reddit.com/r/FULLCOMMUNISM/top/.json"
     req = urllib2.Request(url, headers=headers)
     text_data = urllib2.urlopen(req).read()
     data = json.loads(text_data)
